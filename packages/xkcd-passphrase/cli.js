a9b34a43cd21f8d426447716a5722ab54a874ce4
Ryan Lester <hacker@linux.com>
2022-08-10 22:40:17 -0400

    minor fix

--- packages/xkcd-passphrase/cli.js
+++ packages/xkcd-passphrase/cli.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-require('./dist/xkcd-passphrase').generate(
+require('./dist').generate(
 	parseInt(process.argv[2], 10),
 	process.argv[3] && require('fs').readFileSync(process.argv[3]).toString().trim().split('\n')
 ).then(s => console.log(s));
